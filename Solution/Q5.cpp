3425c5c11cb0dfa564e54324c4fc084197ae0df0
niissshan <nikitashanbhag9@gmail.com>
2024-07-16 13:33:31 +0530

    Add files via upload
    solution

---
+++ Solution/Q5.cpp
@@ -0,0 +1,15 @@
+/*
+Ques: WAP to find the circumference of a 
+circle with radius 10 in C++.
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+float radius;
+radius=2.15;
+float circumference= 2 * 3.145 * radius;
+cout<<circumference<<endl;
+
+
+}
